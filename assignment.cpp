a70f7fc5c2699198c69e3b9ed09ca49baeb87adc
Michal Atlas <michal.z.atlas@gmail.com>
2021-01-08 14:21:52 +0100

    Reversed if_bit_one function to be left

--- assignment.cpp
+++ assignment.cpp
@@ -7,9 +7,11 @@ typedef unsigned char byte;
 //NOTE Exception handling
 
 /// Checks if nth bit from the left is a one
-bool if_bit_one(byte x, int position) {
-    return x & 0x1 << position;
+bool if_bit_one(byte x, uint8_t position) {
+    if (position > 7) { throw std::overflow_error("Checked bit outside range of byte"); }
+    return x & (0x80 >> position);
 }
+
 /// Sets n bits from the left to 0 in an 8 bit byte
 byte trim(byte x, byte trim) {
     return ((UINT8_MAX) >> (trim)) & x;
@@ -22,22 +24,23 @@ int main() {
     std::vector<utf8_point> out;
     utf8_point char_buffer = 0;
     while (std::cin >> std::noskipws >> c) {
-        if (!if_bit_one(c, 7)) { /// Checks if the byte is ascii
+        if (!if_bit_one(c, 0)) { /// Checks if the byte is ascii
             out.push_back(c);
             continue;
         }
-        int leading_ones = 7;
-        while (if_bit_one(c, leading_ones)) leading_ones--; /// Finds how many 1s before the first 0
-        char_buffer = trim(c, 7 - leading_ones); /// Length defining byte
-        for (int i = 7 - leading_ones - 1; i > 0; --i) { //NOTE Tohle fakt nečitelný for loop
+        int leading_ones = 0;
+        while (if_bit_one(c, leading_ones)) leading_ones++; /// Finds how many 1s before the first 0
+        char_buffer = trim(c, leading_ones); /// Length defining byte
+        for (int i = 1; i < leading_ones; ++i) { //NOTE Tohle fakt nečitelný for loop
             if (std::cin.eof()) { return 1; }
             std::cin >> c;
+            if (!(if_bit_one(c, 0) && !if_bit_one(c, 1))) { throw std::logic_error("didn't get continuing byte"); }
             char_buffer = (char_buffer << 6) | trim(c, 0x2);
         }
         out.push_back(char_buffer);
-        char_buffer = 0;
     }
-    for(byte c : out){
-        printf("%u\n", c);
+    for (byte i : out) {
+        printf("%u\n", i);
     }
+    return 0;
 }
