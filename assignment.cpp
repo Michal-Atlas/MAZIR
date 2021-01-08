6703fd6d84f235cf526a02eaab09842a350fd2bd
Michal Atlas <michal.z.atlas@gmail.com>
2021-01-08 17:02:08 +0100

    Minor aesthetic improvement, changed return to throw

--- assignment.cpp
+++ assignment.cpp
@@ -7,7 +7,7 @@ typedef unsigned char byte;
 
 /// Checks if nth bit from the left is a one
 bool if_bit_one(byte x, uint8_t position) {
-    if (position > 7) { throw std::overflow_error("checked bit outside range of byte"); }
+    if (position > 7) throw std::overflow_error("checked bit outside range of byte");
     return x & (0x80 >> position);
 }
 
@@ -29,11 +29,10 @@ int main() {
         int leading_ones = 0;
         while (if_bit_one(c, leading_ones)) leading_ones++; /// Finds how many 1s before the first 0
         char_buffer = trim(c, leading_ones); /// Length defining byte
-        for (; leading_ones > 1; --leading_ones) { 
-            if (std::cin.eof()) { return 1; }
+        for (; leading_ones > 1; --leading_ones) { //NOTE Tohle fakt nečitelný for loop
+            if (std::cin.eof()) throw std::runtime_error("unexpected EOF");
             std::cin >> c;
-	    //NOTE Spíš pomocí masky
-            if (!(if_bit_one(c, 0) && !if_bit_one(c, 1))) { throw std::logic_error("didn't get continuing byte"); }
+            if (!(if_bit_one(c, 0) && !if_bit_one(c, 1))) throw std::logic_error("didn't get continuing byte");
             char_buffer = (char_buffer << 6) | trim(c, 0x2);
         }
         printf("%u\n", char_buffer);
