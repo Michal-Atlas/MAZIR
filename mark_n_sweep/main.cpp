36cc875b29c9a197a0fbfaf76a09b04900d50ffe
Michal Atlas <michal.z.atlas@gmail.com>
2021-02-16 17:05:57 +0100

    Solved Mark and Sweep, tests work
    Signed-off-by: Michal Atlas <michal.z.atlas@gmail.com>

---
+++ mark_n_sweep/main.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+
+#define DEBUG
+
+struct ms_node {
+    std::vector<ms_node *> neighbours;
+    bool marked = false, observed = false;
+
+    void mark_refs() {
+        std::for_each(neighbours.begin(), neighbours.end(), [](ms_node *node) {
+            if (!node->marked) {
+                node->marked = true;
+                node->mark_refs();
+            }
+        });
+    }
+};
+
+int main() {
+    std::vector<ms_node> nodes;
+    { // Initialize NodeList
+        int node_num;
+        std::cin >> node_num;
+        nodes.resize(node_num);
+    }
+    { // Mark Observers
+        int observer_count;
+        std::cin >> observer_count;
+        for (int i = 0; i < observer_count; ++i) {
+            int x;
+            std::cin >> x;
+            nodes[x].observed = true;
+        }
+    }
+    { // Create connections
+        int x, y;
+        while (std::cin >> x >> y) {
+            nodes[x].neighbours.emplace_back(&nodes[y]);
+        }
+    }
+
+    std::for_each(nodes.begin(), nodes.end(), [](ms_node &node) {
+        if (node.observed) {
+            node.marked = true;
+            node.mark_refs();
+        }
+    }); // Mark observers and connected nodes
+
+    std::vector<int> garbage; // Find unmarked Nodes
+    for (int i = 0; i < nodes.size(); ++i) if (!nodes[i].marked) { garbage.emplace_back(i); }
+
+    for (int i : garbage) { std::cout << i << "\n"; }
+    std::cout << std::endl;
+
+    /*std::ostringstream os;
+
+    if (!garbage.empty())
+    {
+        // Convert all but the last element to avoid a trailing ","
+        std::copy(garbage.begin(), garbage.end()-1,
+                  std::ostream_iterator<int>(os, "\n"));
+
+        // Now add the last element with no delimiter
+        os << garbage.back();
+    }*/
+
+    /*std::cout << os.str() << std::endl;
+    for (int i = 0; i < nodes.size(); ++i) {
+        std::cout << "{ " << i << ", O:" << nodes[i].observed << ", M:" << nodes[i].marked << " }" << std::endl;
+    }*/
+}
