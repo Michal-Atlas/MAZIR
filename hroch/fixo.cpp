735bdac6a9340afc49439bd39a0847b8c39c2527
Michal Atlas <michal.z.atlas@gmail.com>
2021-03-03 19:05:58 +0100

    Succeeded all tests, improper fix

--- hroch/fixo.cpp
+++ hroch/fixo.cpp
@@ -1,3 +1,31 @@
 #include "fixo.h"
 
+std::pair<int, int> fixo::pop() {
+    auto first = data;
+    data = data->next;
+    return first->value;
+}
 
+void queue::push(std::pair<int, int> value) {
+    if (!data) { // Push to empty
+        data = std::make_shared<node>(node{value});
+        last = data;
+        return;
+    }
+    if (last == nullptr) { // Initialize on first run
+        last = this->data;
+        while (last->next) {
+            last = last->next;
+        }
+    }
+    last->next = std::make_shared<node>(node{value});
+    last = last->next;
+
+}
+
+void stack::push(std::pair<int, int> value) {
+    auto n = std::make_shared<node>(node{value});
+    auto first = this->data;
+    this->data = n;
+    this->data->next = first;
+}
