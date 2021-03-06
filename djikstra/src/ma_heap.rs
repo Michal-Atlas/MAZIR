466e027700f5215d62d3756003ad1cfc7cb52210
Michal Atlas <michal.z.atlas@gmail.com>
2021-03-06 17:14:09 +0100

    Changed Macros to functions

--- djikstra/src/ma_heap.rs
+++ djikstra/src/ma_heap.rs
@@ -3,24 +3,6 @@ use crate::ma_djikstra::Edge;
 #[derive(Clone, Debug)]
 pub struct Heap(std::vec::Vec<Edge>);
 
-macro_rules! parent {
-    ($i:expr) => {
-        (($i + 1) / 2) - 1
-    };
-}
-
-macro_rules! left_child {
-    ($i:expr) => {
-        (($i + 1) * 2) - 1
-    };
-}
-
-macro_rules! right_child {
-    ($i:expr) => {
-        left_child!($i) + 1
-    };
-}
-
 impl Heap {
     pub fn new() -> Heap {
         Heap(std::vec::Vec::new())
@@ -47,9 +29,9 @@ impl Heap {
     fn bubble_up(&mut self, index: usize) {
         let mut index = index;
         while index > 0 {
-            if self.0[parent!(index)].dist > self.0[index].dist {
-                self.switch(index, parent!(index));
-                index = parent!(index);
+            if self.0[Heap::parent(index)].dist > self.0[index].dist {
+                self.switch(index, Heap::parent(index));
+                index = Heap::parent(index);
             } else {
                 return;
             }
@@ -58,12 +40,12 @@ impl Heap {
     fn bubble_down(&mut self, index: usize) {
         let mut index = index;
         while index < self.0.len() {
-            if self.0[left_child!(index)].dist < self.0[index].dist {
-                self.switch(index, left_child!(index));
-                index = left_child!(index);
-            } else if self.0[right_child!(index)].dist < self.0[index].dist {
-                self.switch(index, right_child!(index));
-                index = right_child!(index);
+            if self.0[Heap::left_child(index)].dist < self.0[index].dist {
+                self.switch(index, Heap::left_child(index));
+                index = Heap::left_child(index);
+            } else if self.0[Heap::right_child(index)].dist < self.0[index].dist {
+                self.switch(index, Heap::right_child(index));
+                index = Heap::right_child(index);
             } else {
                 return;
             }
@@ -78,9 +60,9 @@ impl Heap {
     pub fn insert(&mut self, p: Edge) {
         self.0.push(p);
         let mut index = self.0.len() - 1;
-        while index > 0 && self.0[parent!(index)].dist > self.0[index].dist {
-            self.switch(index, parent!(index)); // Keeps checking if parent! is bigger and switching
-            index = parent!(index);
+        while index > 0 && self.0[Heap::parent(index)].dist > self.0[index].dist {
+            self.switch(index, Heap::parent(index)); // Keeps checking if Heap::parent is bigger and switching
+            index = Heap::parent(index);
         }
     }
     /// Takes value with `key` and updates (by setting not decreasing) the `dist`
@@ -88,5 +70,17 @@ impl Heap {
         let index = self.0.iter_mut().position(|e| e.target == key).unwrap();
         self.0[index].dist = dist;
         self.bubble_up(index);
+    }
+
+    fn parent(i: usize) -> usize {
+        ((i + 1) / 2) - 1
+    }
+
+    fn left_child(i: usize) -> usize {
+        ((i + 1) * 2) - 1
+    }
+
+    fn right_child(i: usize) -> usize {
+        Heap::left_child(i) + 1
     }
 }
